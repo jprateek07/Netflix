280b9fe7663bf49e2c408f53ca1c62bccadde7b8
Prateek3242 <prateek.jain3242@gmail.com>
2021-03-13 03:06:30 +0530

    Error And Responsive Issue

--- src/components/landingscreen/index.jsx
+++ src/components/landingscreen/index.jsx
@@ -4,20 +4,35 @@ function Landingpage() {
   return (
 
     <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+       
       <ol className="carousel-indicators">
+      <button className={`${Style.playbutton} btn btn-outline`} type="submit"><i class="fa fa-play-circle"></i> Play</button>
+      <button className={`${Style.infobutton} btn btn-outline`} type="submit"><i class="fa fa-info-circle"></i> More Info</button>
         <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img className="d-block w-100" src="/assets/img/movi1.jpg" height={500} alt="First slide" />
+          <img className={`${Style.images} d-block w-100`} src="/assets/img/movie7.jpg" height={700} alt="First slide" />
+          <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+          <h5>MONEY HEIST</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
         </div>
         <div className="carousel-item">
-          <img className="d-block w-100" src="assets/img/movie2.jpg" height={500} alt="Second slide" />
+          <img className={`${Style.images} d-block w-100`} src="assets/img/movie2.jpg" height={700} alt="Second slide" />
+          <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+          <h5>THE WITCHER</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
         </div>
         <div className="carousel-item">
-          <img className="d-block w-100" src="assets/img/movie3.jpg" height={500} alt="Third slide" />
+          <img className={`${Style.images} d-block w-100`} src="assets/img/movie4.jpg" height={700} alt="Third slide" />
+          <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+          <h5>GAME OF THRONES</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
         </div>
       </div>
       <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
