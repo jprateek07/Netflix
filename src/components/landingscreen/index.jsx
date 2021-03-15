ec3e8603029d28a648a0e120a23f765bb4a8f104
Prateek3242 <prateek.jain3242@gmail.com>
2021-03-15 13:03:58 +0530

    logo changed

--- src/components/landingscreen/index.jsx
+++ src/components/landingscreen/index.jsx
@@ -2,37 +2,43 @@ import React from 'react'
 import Style from './index.module.css'
 function Landingpage() {
   return (
-
     <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-       
       <ol className="carousel-indicators">
-      <button className={`${Style.playbutton} btn btn-outline`} type="submit"><i class="fa fa-play-circle"></i> Play</button>
-      <button className={`${Style.infobutton} btn btn-outline`} type="submit"><i class="fa fa-info-circle"></i> More Info</button>
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <button className={`${Style.playbutton} btn btn-outline`} type="submit"><i class="fa fa-play-circle"></i> Play</button>
+        <button className={`${Style.infobutton} btn btn-outline`} type="submit"><i class="fa fa-info-circle"></i> More Info</button>
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
       </ol>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img className={`${Style.images} d-block w-100`} src="/assets/img/movie7.jpg" height={700} alt="First slide" />
-          <div className={`${Style.info} carousel-caption d-none d-md-block`}>
-          <h5>MONEY HEIST</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-        </div>
-        </div>
-        <div className="carousel-item">
-          <img className={`${Style.images} d-block w-100`} src="assets/img/movie2.jpg" height={700} alt="Second slide" />
-          <div className={`${Style.info} carousel-caption d-none d-md-block`}>
-          <h5>THE WITCHER</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-        </div>
+          <img className={`${Style.images} d-block w-100`} src="../assets/img/movie1.jpg" height={700} alt="First slide" />
+            <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+              <h5>MONEY HEIST</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+            </div>
         </div>
         <div className="carousel-item">
-          <img className={`${Style.images} d-block w-100`} src="assets/img/movie4.jpg" height={700} alt="Third slide" />
-          <div className={`${Style.info} carousel-caption d-none d-md-block`}>
-          <h5>GAME OF THRONES</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+          <img className={`${Style.images} d-block w-100`} src="../assets/img/movie2.jpg" height={700} alt="Third slide" />
+            <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+              <h5>LUCIFER</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+            </div>
         </div>
+        <div className="carousel-item">
+          <img className={`${Style.images} d-block w-100`} src="../assets/img/movie3.jpg" height={700} alt="Third slide" />
+            <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+              <h5>GAME OF THRONES</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div className="carousel-item">
+          <img className={`${Style.images} d-block w-100`} src="../assets/img/movie4.jpg" height={700} alt="Second slide" />
+            <div className={`${Style.info} carousel-caption d-none d-md-block`}>
+              <h5>THE WITCHER</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+            </div>
         </div>
       </div>
       <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -43,7 +49,6 @@ function Landingpage() {
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="sr-only">Next</span>
       </a>
-
     </div>
   )
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,58 +1,45 @@
 import React from 'react'
+import { useEffect, useState } from "react";
 import Style from './index.module.css'
+import {usestate} from 'react'
 function Navbar() {
+  const [searchTerm, setSearchTerm] =useState('')
   return (
     <nav className={`${Style.navbg} navbar navbar-expand-lg navbar-dark`}>
-      <a className="navbar-brand" href="#"><img src="../assets/img/netlogo.png" alt="Logo" /></a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarText">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item ">
-            <a className={`${Style.navtext} nav-link`} href="#home">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className={`${Style.navtext} nav-link`} href="#tv">TV Shows</a>
-          </li>
-          <li className="nav-item">
-            <a className={`${Style.navtext} nav-link`} href="#movies">Movies</a>
-          </li>
-          <li className="nav-item">
-            <a className={`${Style.navtext} nav-link`} href="#originals">Originals</a>
-          </li>
-          <li className="nav-item">
-            <a className={`${Style.navtext} nav-link`} href="#movies">Recently Added</a>
-          </li>
-          <li className="nav-item">
-            <a className={`${Style.navtext} nav-link`} href="#home">Portfolio</a>
-          </li>
-        </ul>
-        <span className="navbar-text">
+      <a className="navbar-brand" href="#"><img src="../assets/img/logo movie.png" alt="Logo" /></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav mr-auto">
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-danger my-2 my-sm-0" type="submit"><i class="fa fa-search"></i>Search</button>
-            </form>
-            <li className="nav-item">
-              <a className={`${Style.navtext} nav-link`} href="#"><i className="fa fa-bell"></i></a>
-            </li>
-            <li className="nav-item">
-            <div className="dropdown">
-                <button className={`${Style.account} btn dropdown-toggle`} type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Account
-                  </button>
-                <div className={`${Style.accmenu} dropdown-menu`} aria-labelledby="dropdownMenuButton">
-                  <a className={`${Style.accinfo} dropdown-item`}>PRTAEEK NALWAYA</a>
-                  <a className={`${Style.accinfo} dropdown-item`}>MY ACCOUNT</a>
-                  <a className={`${Style.accinfo} dropdown-item`}>LOG OUT</a>
-                </div>
-              </div>
-            </li>
+            <li className="nav-item "><a className={`${Style.navtext} nav-link`} href="#home">Home</a></li>
+            <li className="nav-item"><a className={`${Style.navtext} nav-link`} href="#tv">TV Shows</a></li>
+            <li className="nav-item"><a className={`${Style.navtext} nav-link`} href="#movies">Movies</a></li>
+            <li className="nav-item"><a className={`${Style.navtext} nav-link`} href="#originals">Originals</a></li>
+            <li className="nav-item"><a className={`${Style.navtext} nav-link`} href="#movies">Recently Added</a></li>
+            <li className="nav-item"><a className={`${Style.navtext} nav-link`} href="#home">Portfolio</a></li>
           </ul>
-        </span>
-      </div>
-    </nav>
-  )
+          <span className="navbar-text">
+            <ul className="navbar-nav mr-auto">
+              <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(event) => {setSearchTerm(event.target.value)}} />
+                <button className={`${Style.search} btn my-2 my-sm-0`} type="submit"><i class="fa fa-search"></i>Search</button>
+              </form>
+              <li className="nav-item"><a className={`${Style.navtext} nav-link`} href="#"><i className="fa fa-bell"></i></a></li>
+              <li className="nav-item">
+                <div className="dropdown">
+                  <button className={`${Style.account} btn dropdown-toggle`} type="button" id="dropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</button>
+                    <div className={`${Style.accmenu} dropdown-menu`} aria-labelledby="dropDown">
+                      <a className={`${Style.accinfo} dropdown-item`}>PRTAEEK NALWAYA</a>
+                      <a className={`${Style.accinfo} dropdown-item`}>MY ACCOUNT</a>
+                      <a className={`${Style.accinfo} dropdown-item`}>LOG OUT</a>
+                    </div>
+                </div>
+              </li>
+            </ul>
+          </span>
+        </div>
+      </nav>
+    )
 }
 export default Navbar;
